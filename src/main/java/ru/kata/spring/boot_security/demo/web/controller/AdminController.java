be30296bb8e0a533128d28bfbe33cd311a3c0020
HP OMEN <nord2002d@yandex.ru>
2023-08-29 02:25:42 +0300

    initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/web/controller/AdminController.java
@@ -0,0 +1,35 @@
+package ru.kata.spring.boot_security.demo.web.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.web.model.User;
+import ru.kata.spring.boot_security.demo.web.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+public class AdminController {
+    private UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public String startPage(){
+        return "login";
+    }
+    @GetMapping("/admin")
+    public String adminPage( Principal principal, ModelMap model) throws ParseException, JsonProcessingException {
+        String email = userService.getEmailAuthentication(principal);
+        User user = userService.findByEmailLike(email);
+        model.addAttribute("adminUser",user);
+        model.addAttribute("admin",userService.listUsers());
+        return "admin";
+    }
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/web/controller/RestController.java
@@ -0,0 +1,54 @@
+package ru.kata.spring.boot_security.demo.web.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.web.exeptions.UserEmailException;
+import ru.kata.spring.boot_security.demo.web.exeptions.UserNotFoundException;
+import ru.kata.spring.boot_security.demo.web.model.User;
+import ru.kata.spring.boot_security.demo.web.service.UserService;
+
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("admin/form")
+public class RestController {
+    private UserService userService;
+
+    public RestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("{userId}")
+    public  User getUser(@PathVariable long userId) throws UserNotFoundException {
+        return userService.getUser(userId);
+    }
+    @PostMapping
+    public User addUser(@RequestBody User user) throws UserEmailException {
+        User addUser = new User();
+        addUser.setUsername(user.getUsername());
+        addUser.setSurName(user.getSurName());
+        addUser.setAge(user.getAge());
+        addUser.setEmail(user.getEmail());
+        addUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        addUser.setRoles(user.getRoles());
+        return userService.add(addUser);
+    }
+
+    @PatchMapping
+    public User updateUser(@RequestBody User user) throws UserEmailException {
+        User userUpdate = new User();
+        userUpdate.setId(user.getId());
+        userUpdate.setUsername(user.getUsername());
+        userUpdate.setSurName(user.getSurName());
+        userUpdate.setAge(user.getAge());
+        userUpdate.setEmail(user.getEmail());
+        userUpdate.setPassword(user.getPassword());
+        userUpdate.setRoles(user.getRoles());
+        return userService.update(userUpdate);
+    }
+
+    @DeleteMapping
+    public User deleteUser(@RequestBody User user) throws UserNotFoundException {
+        userService.removeUser(user.getId());
+        return new User();
+    }
+}
