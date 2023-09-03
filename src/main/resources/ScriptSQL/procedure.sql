1ffb90a89c287e627a73acd6d72e5b8baf2380be
HP OMEN <nord2002d@yandex.ru>
2023-09-03 19:52:22 +0300

    Изменил SQL script, Удалили Util класс.

--- src/main/resources/ScriptSQL/procedure.sql
+++ src/main/resources/ScriptSQL/procedure.sql
@@ -1,19 +1,15 @@
-DELIMITER //
 
-CREATE PROCEDURE create_users(
-             IN   id bigint,
-             IN   `age` int,
-             IN  email  varchar(60),
-             IN  username  varchar(30),
-             IN  `password` varchar(255),
-             IN  sur_name varchar(30),
-             IN  roles varchar(30)
-)
-BEGIN
-    REPLACE INTO users VALUES ( id,`age`, email, username, `password`, sur_name);
-    DELETE FROM user_role where id = 1 LIMIT 1000;
-    DELETE FROM user_role where id = 1 LIMIT 1000;
-    REPLACE INTO user_role VALUES ( id, roles);
 
-END;
-DELIMITER ;
+#       Вставка в новосозданныую таблицу
+
+INSERT INTO users VALUES (1, 11,'admin@a.ru', 'admin', '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq', 'Sokolov');
+INSERT INTO users VALUES (2, 12, 'user@u.ru', 'user', '$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC', 'Petrov');
+INSERT INTO user_role VALUES (1, 'ADMIN');
+INSERT INTO user_role VALUES (2, 'USER');
+
+
+#       Вставка в таблицу созданную в задании 3.1.3
+
+# REPLACE INTO users VALUES (1, 11,'admin', '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq', 'Sokolov','admin@a.ru');
+# REPLACE INTO users VALUES (2, 12, 'user', '$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC', 'Petrov','user@u.ru');
+
